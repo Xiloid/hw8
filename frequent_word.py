6e6e34a9edca63629e3c66c83740b871f0caf70a
Xiloid <faustov.roman.a@gmail.com>
2021-02-24 19:55:34 +0200

    commited

--- frequent_word.py
+++ frequent_word.py
@@ -9,7 +9,24 @@
     # чаще всего встречаются "hi", "i" и "am", но по алфавиту "am" - раньше
     assert frequent_word(text) == "am"
 """
+text = "hi world, hi python. i am very cool but i am still learning."
+
+
+def main():
+    print_list = frequent_word(text)
+    print('Список наиболее часто встречающихся слов по алфавиту:')
+    print(print_list)
 
 
 def frequent_word(text):
-    pass
+    tmp_dict = {}
+    for a in text.split(' '):
+        for b in a.split():
+            tmp_dict[b] = tmp_dict.get(b, 0) + 1
+    most_word = tmp_dict[max(tmp_dict, key=lambda i: tmp_dict[i])]
+    out_list = (sorted([j for j in tmp_dict if tmp_dict[j] == most_word]))
+    return out_list
+
+
+if __name__ == "__main__":
+    main()
