595760ff2ec2202e01a449be52821feabc5470a6
Xiloid <faustov.roman.a@gmail.com>
2021-02-24 20:09:40 +0200

    commited

--- frequent_word.py
+++ frequent_word.py
@@ -25,7 +25,10 @@ def frequent_word(text):
             tmp_dict[b] = tmp_dict.get(b, 0) + 1
     most_word = tmp_dict[max(tmp_dict, key=lambda i: tmp_dict[i])]
     out_list = (sorted([j for j in tmp_dict if tmp_dict[j] == most_word]))
-    return out_list
+    return out_list[0]
+
+
+assert frequent_word(text) == 'am'
 
 
 if __name__ == "__main__":
