9de5346fe0f9502741e2ed42bb17e2bb9e020447
Xiloid <faustov.roman.a@gmail.com>
2021-02-24 18:57:15 +0200

    commited

--- country_city.py
+++ country_city.py
@@ -17,38 +17,31 @@ data = {
 
 
 def main():
-    final_list = groupping_data(data)
-    print(final_list)
-
-'''    city = input('Введите название города: ')
+    city = input('Введите название города: ')
     city_result = get_country(city)
     print(f'Страна, которой принадлежит город {city}: {city_result}')
+    print()
+    final_list = groupping_data(data)
+    print('Отформатированный список согласно условия:')
+    print(groupping_data(data))
 
-   
 
 def get_country(city):
     for key, value in data.items():
         for j in value:
             if j == city:
                 return key
-'''
-# {'country': 'Ukraine', 'capital': 'Kiev', 'cities': ['Kharkiv', 'Odesa', 'Dnipro']}
+
+
 def groupping_data(data):
     list = []
-    a_country = b_capital = c_cities = data_final = {}
     for key, val in data.items():
         a_country = {'country': key}
         b_capital = {'capital': val[0]}
         c_cities = {'cities': val[1:4]}
         data_final = a_country | b_capital | c_cities
         list.append(data_final)
-
-
-#       print(data_final)
-    #print(list)
-
     return list
-
 
 
 if __name__ == "__main__":
